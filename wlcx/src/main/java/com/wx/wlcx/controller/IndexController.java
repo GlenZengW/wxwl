dd0f5890e983706e263285f8c4cfa1c93c0c6e8c
Glen Zeng <18397641397@163.com>
2018-11-26 00:03:31 +0800

    fixed 404 error

--- wlcx/src/main/java/com/wx/wlcx/controller/IndexController.java
+++ wlcx/src/main/java/com/wx/wlcx/controller/IndexController.java
@@ -1,14 +1,28 @@
 package com.wx.wlcx.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+//@Controller
+//public class IndexController {
+//
+//    @RequestMapping("/test")
+//    public String index() {
+//        System.out.println("/index");
+//        return "index";
+//    }
+//}
+
+@RestController
 public class IndexController {
 
-    @RequestMapping("/test")
-    public String index() {
-        System.out.println("/index");
-        return "index";
+    @GetMapping("/test")
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        return mv;
     }
 }
