e4a97e31c26888f0187096b1720c984f0acaf119
Glen Zeng <18397641397@163.com>
2018-12-08 22:32:54 +0800

    集成swagger2

--- wlcx/src/main/java/com/wx/wlcx/WlcxApplication.java
+++ wlcx/src/main/java/com/wx/wlcx/WlcxApplication.java
@@ -3,9 +3,11 @@ package com.wx.wlcx;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("com.wx.wlcx.mapper")
+@EnableSwagger2
 public class WlcxApplication {
 
 	public static void main(String[] args) {
